25cdfc2b26f2c1a2ab848ab8ce1af137fc65c6e1
Rubenwardy <anjayward@gmail.com>
2013-02-23 14:44:23 +0000

    add real achievements

--- init.lua
+++ init.lua
@@ -6,6 +6,28 @@
 
 dofile(minetest.get_modpath("awards").."/api.lua")
 
+-- Light it up
+awards.register_achievement("award_lightitup",{
+	title = "Light It Up",
+	description = "You have placed 100 torches",
+	trigger={
+		type="dig",
+		node="default:tree",
+		target=100,
+	},
+})
+
+-- Lumber Jack
+awards.register_achievement("award_lumberjack",{
+	title = "Lumber Jack",
+	description = "You have dug 100 tree blocks",
+	trigger={
+		type="dig",
+		node="default:torch",
+		target=100,
+	},
+})
+
 -- Found some Mese!
 awards.register_achievement("award_mesefind",{
 	title = "First Mese Find",
@@ -17,17 +39,46 @@ awards.register_achievement("award_mesefind",{
 	},
 })
 
-awards.register_onDig(function(player,data)
-	return nil
-end)
-
--- First Brick Placed!
-awards.register_achievement("award_foundations",{
-	title = "Foundations",
-	description = "Every house starts from its foundations!",
+-- Just entered the mine
+awards.register_achievement("award_mine1",{
+	title = "Just Entered the mine",
+	description = "You have dug 10 stone blocks",
 	trigger={
-		type="place",
-		node="default:brick",
-		target=1,
+		type="dig",
+		node="default:stone",
+		target=10,
+	},
+})
+
+-- Mini Miner
+awards.register_achievement("award_mine2",{
+	title = "Mini Miner",
+	description = "You have dug 100 stone blocks",
+	trigger={
+		type="dig",
+		node="default:stone",
+		target=100,
+	},
+})
+
+-- Hardened Miner
+awards.register_achievement("award_mine3",{
+	title = "Hardened Miner",
+	description = "You have dug 1000 stone blocks",
+	trigger={
+		type="dig",
+		node="default:stone",
+		target=1000,
+	},
+})
+
+-- Master Miner
+awards.register_achievement("award_mine4",{
+	title = "Master Miner",
+	description = "You have dug 10000 stone blocks",
+	trigger={
+		type="dig",
+		node="default:stone",
+		target=10000,
 	},
 })
