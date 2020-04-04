8a34ea80553b51d94e2cb2243501f2df71ebe4aa
Beha <shacknetisp@mail.com>
2020-04-04 13:46:41 -0400

    Add awards.add_defaults setting to control adding default awards. Useful for games not based on MTG but using some of its mods.

--- init.lua
+++ init.lua
@@ -16,7 +16,11 @@ dofile(minetest.get_modpath("awards").."/src/api_triggers.lua")
 dofile(minetest.get_modpath("awards").."/src/chat_commands.lua")
 dofile(minetest.get_modpath("awards").."/src/gui.lua")
 dofile(minetest.get_modpath("awards").."/src/triggers.lua")
-dofile(minetest.get_modpath("awards").."/src/awards.lua")
+
+-- Optionally add default awards.
+if minetest.settings:get_bool("awards.add_defaults", true) then
+	dofile(minetest.get_modpath("awards").."/src/awards.lua")
+end
 
 awards.load()
 minetest.register_on_shutdown(awards.save)
